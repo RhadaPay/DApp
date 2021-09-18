ee574c9604cab6ac0685bfc827040f4b28f88579
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-09-18 14:35:20 +0200

    Add some dummy menu items to the navbar

--- src/App.vue
+++ src/App.vue
@@ -19,9 +19,24 @@
     </v-app-bar>
 
     <v-main>
-      <v-navigation-drawer
-        v-model="drawer"
-      >
+      <v-navigation-drawer v-model="drawer">
+        <v-list v-if="errorType == null">
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+            :to="item.to"
+            router
+            exact
+            style="z-index: 1000"
+          >
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
       </v-navigation-drawer>
       <div>
         <router-view />
@@ -33,4 +48,8 @@
 import ConnectButton from "./components/ConnectButton.vue";
 import { ref } from "vue";
 const drawer = ref(true);
+const items = ref([
+  { icon: "mdi-magnify", title: "Menu Item 1", to: "/" },
+  { icon: "mdi-plus-box", title: "Menu Item 2", to: "/" },
+]);
 </script>
