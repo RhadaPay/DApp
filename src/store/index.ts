db5d5e9fe20655ce88e9f1fd7833e29da6281eb0
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-09-18 19:53:01 +0200

    First working version of wallet connection

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,10 @@
 import { InjectionKey } from "vue";
 import { createStore, Store } from "vuex";
+import web3 from "./modules/web3";
 
-interface storeTypes {
-}
+interface storeTypes {}
 export const key: InjectionKey<Store<storeTypes>> = Symbol();
 
-export const store = createStore<storeTypes>({});
+export const store = createStore<storeTypes>({
+  modules: { web3 },
+});
