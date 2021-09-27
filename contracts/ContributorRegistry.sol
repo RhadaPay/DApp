876355d20970d539086adb3f647d9537ae5abe20
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-09-27 19:25:48 +0200

    Add register function

--- contracts/ContributorRegistry.sol
+++ contracts/ContributorRegistry.sol
@@ -3,4 +3,37 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ContributorRegistry is Ownable{
+    uint8 public requiredConfirmations;
+
+    enum Status {NONE, REGISTERED, CONFIRMED}
+    struct Contributor {
+        Status status;
+        uint8 confirmations;
+        string discordHandle;
+        string githubUsername;
+    }
+    mapping (address => Contributor) public contributors;
+    mapping (string => bool) registeredDiscordHandles;
+    mapping (string => bool) registeredGithubUsernames;
+
+    constructor(uint8 _requiredConfirmations) public {
+        requiredConfirmations = _requiredConfirmations;
+    }
+
+    event Registered(address contributor, string discordHandle, string githubUsername);
+
+    function register(string memory _discordHandle, string memory _githubUsername) public {
+        require(contributors[msg.sender].status == Status.NONE, "Account has registered already");
+        require(!registeredDiscordHandles[_discordHandle], "Discord handle is already registered");
+        require(!registeredGithubUsernames[_githubUsername], "Github username is already registered");
+
+        registeredDiscordHandles[_discordHandle] = true;
+        contributors[msg.sender].discordHandle = _discordHandle;
+
+        registeredGithubUsernames[_githubUsername] = true;
+        contributors[msg.sender].githubUsername = _githubUsername;
+
+        contributors[msg.sender].status = Status.REGISTERED;
+        emit Registered(msg.sender, _discordHandle, _githubUsername);
+    }
 }
--- test/contributorRegistry.spec.js
+++ test/contributorRegistry.spec.js
@@ -2,16 +2,37 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("ContributorRegistry", function () {
+  const requiredConfirmations = 2;
+  let contributorRegistry;
+  let contributorData = {};
+
+  let owner, firstContributor;
   before(async function () {
-    [owner] = await ethers.getSigners();
+    [owner, firstContributor] = await ethers.getSigners();
+    contributorData[firstContributor.address] = {
+      discordHandle: "firstDiscord",
+      githubUsername: "firstGithub",
+    };
   });
   it("Should return the correct owner", async function () {
     const ContributorRegistry = await ethers.getContractFactory(
       "ContributorRegistry"
     );
-    const contributorRegistry = await ContributorRegistry.deploy();
+    contributorRegistry = await ContributorRegistry.deploy(
+      requiredConfirmations
+    );
     await contributorRegistry.deployed();
 
     expect(await contributorRegistry.owner()).to.equal(owner.address);
+  });
+  it("Should return the correct number of required confirmations", async function () {
+    expect(await contributorRegistry.requiredConfirmations()).to.equal(
+      requiredConfirmations
+    );
+  });
+  it("Should be able to register", async function () {
+    console.log(contributorData)
+    const callData = contributorData[firstContributor.address];
+    await contributorRegistry.connect(firstContributor).register(callData.discordHandle, callData.githubUsername)
   });
 });
