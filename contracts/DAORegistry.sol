83fcf634edd9ba644462f84e13dbe45f1a85d6d5
Levi <48064973+leviwillms@users.noreply.github.com>
2021-10-03 13:28:32 -0400

    Initial DAORegistry Skeleton

---
+++ contracts/DAORegistry.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ContributorRegistry.sol";
+import "./RoundVoting.sol";
+
+contract DAORegistry is Ownable {
+    /* ============ Datatypes ============ */
+    struct DAO {
+        Contributer[] registry; // this wont work but I want to have an array of contributors.
+                                 // But maybe that isn't nesessary for registration. I may have it backwards.. We probably want to connect the DAO contract
+                                 // to the other contracts, not the other contracts to the DAO
+                                 // This could also be just a list of members, e.g. adress[] members;
+        address daoAddress;
+        string daoName;
+        bool approved; // my idea for this is that a member of Rhada can change the value of this field to approve a DAO application
+        address roundManager;
+        address[] admins;
+    }
+
+    /* ============ State Variables ============ */
+    // List of DAOs
+    DAO[] public DAOs;
+    // list of Contributors in a DAO (will have to think of a method to expose this list)
+    Contributor[] contributors;
+    // The streaming instance of a DAO
+    // PaymentStream public stream;
+    // The voting rounds of a DAO
+    RoundVoting public round;
+    // The registry of a DAO
+    ContributorRegistry public registry;
+
+    /**
+        Constructor Function
+        @param streamAddress The contract address used to manage payment streams.
+        @param roundAddress The contract address where round voting details can be found.
+        @param registryAddress The contract address where registered contributors and confirmed
+                                members can be found.
+     */
+    constructor (address streamAddress, address roundAddress, address registryAddress) {
+        // stream = PaymentStream(streamAddress);
+        round = RoundVoting(roundAddress);
+        registry = ContributorRegistry(registryAddress);
+    }
+
+    /* Private Functions */ 
+    function registerDAO() {
+
+    }
+
+    function updateStream() {
+
+    }
+
+    function addContributor() {
+
+    }
+
+
+}
