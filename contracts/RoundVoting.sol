ee37989704e46d1cb0c6c92a6b562784ea293b4b
k-agau <ksilvergold18@gmail.com>
2021-10-06 09:25:34 -0500

    Update RoundVoting.sol

--- contracts/RoundVoting.sol
+++ contracts/RoundVoting.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-//import "@openzeppelin/contracts/access/Ownable.sol";
-import "./ContributorRegistry.sol";
+// import "./ContributorRegistry.sol";
 /**
     Simple implementation of a voting contract done on-chain.
     This contract allows confirmed members to place votes for a particular member.
@@ -14,8 +13,9 @@ import "./ContributorRegistry.sol";
         * Implement ability to modify the timeout
         * Implement weighted voting
         * Implement group voting? Can just give people the ability to vote for multiple rounds at once to save on space
+        * Different structs for different types of voting (more expensive deployment -> save lots for users)
  */
-contract RoundManager is Ownable {
+contract RoundManager {
 
     /* Data */
     enum Status {
@@ -26,31 +26,33 @@ contract RoundManager is Ownable {
 
     struct Round {
         Status status;
-        address newUser;
-        uint128 numVotes;
-        uint128 endTime; // What uint does time use again
+        uint roundSalary;
     }
 
     /* ============ State Variables ============ */
     // Stores rounds
     Round[] public rounds;
     // The registry of a specific DAO
-    ContributorRegistry public registry;
+    // ContributorRegistry public registry;
     // List of addresses with admin privileges for a given DAO
     address[] public admins;
+    // List of the addresses in the current round
+    mapping(uint => address[]) usersInRound;
+    // Number of votes for a user in a given round
+    mapping(uint => mapping(address => uint)) numVotes;
     // Mapping of round to those who have voted in that round
     mapping(uint => mapping(address => bool)) public hasVoted; // Or just map uint to address and iterate through?
     // TEMPORARY MAPPING
-    mapping(address => uint) weightedVoting;
+    mapping(address => uint) weightedVoting; // Use weighted voting identifier as way to communicate w registry about who can/can't vote
     // Timeout
-    uint256 timeout;
+    uint256 timePerRound;
 
     /* Events */
     event VoteCast(address _for, address _by);
-    event RoundOpen(uint roundId);
-    event RoundCompleted(uint roundId);
-    event RoundCancelled(uint roundId);
-    event VotesCalculated(uint roundId);
+    event RoundOpen(uint roundID);
+    event RoundCompleted(uint roundID);
+    event RoundCancelled(uint roundID);
+    event VotesCalculated(uint roundID);
 
     /* Modifiers */
 
@@ -64,23 +66,13 @@ contract RoundManager is Ownable {
         _;
     }
 
-    modifier userHasNotVoted(uint roundId) {
-        require(!hasVoted[roundID][msg.sender], "Member has already voted");
+    modifier canVote(uint256 roundID) {
+        require(!hasVoted[roundID][msg.sender] && weightedVoting[msg.sender] > 0);
         _;
     }
 
-    /** 
-        Calls the ContributorRegistry contract to validate the user
-        has the right to vote, based on whether they are registered or not.
-    */
-    modifier canVote() {
-        require(weightedVoting[msg.sender] > 0);
-        _;
-    }
-
-    modifier roundIsOpen(uint roundId) {
-        _checkTime(roundId);
-        require(rounds[roundId].status == Status.Open, "Round is not open");
+    modifier roundIsOpen(uint256 roundID) {
+        require(rounds[roundID].status == Status.Open, "Round is not open");
         _;
     }
 
@@ -89,17 +81,10 @@ contract RoundManager is Ownable {
         @param registryAddress: this is the contract address where the list of registered
             and confirmed members of the dao can be found
     */     
-    constructor(address registryAddress, uint256 _timeout) {
-        registry = ContributorRegistry(registryAddress);
+    constructor(address registryAddress, uint256 _timePerRound) {
+        //registry = ContributorRegistry(registryAddress);
         admins.push_back(msg.sender);
-        timeout = _timeout;
-    }
-
-    /* Private Methods */
-    function _checkTime(uint256 roundId) private {
-        if(block.timestamp > endtime) {
-            rounds[roundId].status = Status.Closed;
-        }
+        timePerRound = _timePerRound;
     }
 
     /* Public Methods */
@@ -108,27 +93,29 @@ contract RoundManager is Ownable {
         Creates a new round of voting
     */
     function openRound(
-        address _newUser
+        address[] memory newUsers, uint256 _roundSalary
     ) public {
         rounds.push_back(Round({
-            status: State.Open,
-            newUser: _newUser,
-            numVotes: 0,
-            endTime: block.timestamp + timeout
+            status: Status.Open,
+            roundSalary: _roundSalary
         }));
+        usersInRound[rounds.size - 1] = newUsers;
         // emit RoundOpened();
     }
 
     /**
-        @param roundId: numerical identifier found in the Round struct
+        @param roundID: numerical identifier found in the Round struct
         @param _for: the address of the person for whom you're voting
      */
     function castVote(
-        uint roundId,
-        address _for
-    ) public isConfirmedUser(_for) isAllowedToVote roundIsOpen(roundId) userHasNotVoted(roundId) {
-        hasVoted[roundId][msg.sender] = true;
-        rounds[roundID].numVotes += 1; // add safe math
+        uint256 roundID,
+        address[] memory _for
+    ) public isConfirmedUser(_for) roundIsOpen(roundID) {
+        hasVoted[roundID][msg.sender] = true;
+        for(int i = 0; i < _for.size; i++) {
+            numVotes[roundID][msg.sender] += weightedVoting[msg.sender];
+        }
+        
         // emit VoteCast();
     }
 
@@ -136,11 +123,9 @@ contract RoundManager is Ownable {
         Closes an existing round of voting
      */
     function closeRound(
-        uint roundId
+        uint roundID
     ) public {
-        _updateTime(roundId);
-        require(rounds[roundId].status == Status.Closed, "The voting blocl is not closed");
-        calculateVotes(roundId);
+        require(rounds[roundID].status == Status.Closed, "The voting blocl is not closed");
         // emit RoundClosed();
     }
 }
