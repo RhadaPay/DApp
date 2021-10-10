c684d9d2a68a3d1447f0876f1ee3cd6ba13b8c3b
Francesco Moca <francesco.moca@gmail.com>
2021-10-10 17:13:27 +0200

    deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,11 +5,17 @@ import 'hardhat-deploy';
 
 import { HardhatUserConfig } from "hardhat/config";
 
+require("dotenv").config();
 
 const config: HardhatUserConfig =  {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {},
+    mumbai: {
+      url: process.env.MUMBAI_RPC,
+      chainId: 80001,
+      accounts: [ process.env.PRIVATE_KEY as string]
+    }
   },
   solidity: {
     version: "0.8.0",
@@ -28,6 +34,18 @@ const config: HardhatUserConfig =  {
   },
   mocha: {
     timeout: 20000
+  },
+  namedAccounts: {
+    deployer: 0,
+    host: {
+      "mumbai": '0xEB796bdb90fFA0f28255275e16936D25d3418603'
+    },
+    cfa: {
+      "mumbai": '0x49e565Ed1bdc17F3d220f72DF0857C26FA83F873'
+    },
+    acceptedToken: {
+      "mumbai": '0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f'
+    }
   },
 };
 
